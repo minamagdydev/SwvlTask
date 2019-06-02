6fa253b78ba8dafdc5bbb28fe9ebf2c65d78897f
mina M <minamagdyfci@gmail.com>
2019-06-02 17:20:49 +0200

    Initial Commit

---
+++ SwvlTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwvlTask
+//
+//  Created by lujin sherif  on 6/2/19.
+//  Copyright © 2019 minamagdy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
